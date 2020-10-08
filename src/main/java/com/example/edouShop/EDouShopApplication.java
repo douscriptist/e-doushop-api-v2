14673273f4c7014ac0ce4a6b36eed82403285f6e
Kaan Yilmaz <douscriptist@gmail.com>
2020-10-08 03:14:33 +0300

    init spring backend

---
+++ src/main/java/com/example/edouShop/EDouShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.edouShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDouShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDouShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/edouShop/EDouShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edouShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDouShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
